7b829b4cc8aa2ca7ef3d9c996cef0417bdbe812d
unknown <nguyenhongtinnn@gmail.com>
2026-09-17 09:13:38 +0700

    Add deploy configuration for Render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=ypk^5ih=s91dfcf15k@_6e8cz_%5d(pz%^t&=-32ji@n+$5^q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
