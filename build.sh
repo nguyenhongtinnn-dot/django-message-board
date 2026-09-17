1103f8089cffd3aa3747a92c6ef700647e70260f
unknown <nguyenhongtinnn@gmail.com>
2026-09-17 09:26:34 +0700

    Fix py command to python in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-py -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-py manage.py collectstatic --no-input
-py manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
