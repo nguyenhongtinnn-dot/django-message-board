4205780bb764f5a753b4c0252faccac34f0e60ea
unknown <nguyenhongtinnn@gmail.com>
2026-09-17 12:25:35 +0700

    Complete unit tests and static files setup

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(default="hello")
 
     def __str__(self):
         return self.text[:50]
